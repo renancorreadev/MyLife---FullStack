d2db87ab768b7504975675e1b649f5fb2567cf41
renancorreadev <producer@skynance.net>
2023-01-19 23:35:24 -0300

    chore: configure eslint, husky, prettier to project

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,28 +1,28 @@
 import Fastify from 'fastify';
-import {PrismaClient} from '@prisma/client'
-import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
 
 const app = Fastify();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 app.register(cors, {
   origin: '*',
-})
+});
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany({
     where: { 
       title : {
-        startsWith: "Beber"
+        startsWith: 'Beber'
       }
     }
-  })
+  });
 
-  return habits
-})
+  return habits;
+});
 
 app.listen({
   port: 3000,
 }).then(() => {
-  console.log("Server is running 🎸")
-})
+  console.log('Server is running 🎸');
+});
